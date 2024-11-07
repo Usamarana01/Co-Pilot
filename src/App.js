8b3698e42cda6e77f23291409d61e0bbf0451b94
Muhammad Usama <91366259+Usamarana01@users.noreply.github.com>
2024-11-07 22:36:04 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import chat from './chatbotpage'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <chat />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/chatbotpage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Chatbot from "./chatbot";
+const chat = ()=> {
+    return <>
+    <Chatbot />
+    </>
+
+}
